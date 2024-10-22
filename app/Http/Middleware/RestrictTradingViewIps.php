c180b069093f1440dbca129d7334591a29002b35
Petro Chaikivskyi <pchaikivskyi@gmail.com>
2024-10-22 15:47:13 +0300

    Never restrict ips for local env

--- app/Http/Middleware/RestrictTradingViewIps.php
+++ app/Http/Middleware/RestrictTradingViewIps.php
@@ -14,7 +14,7 @@ class RestrictTradingViewIps
         $allowedIps = config()->array('services.trading-view.ips');
         $clientIp = $request->ip();
 
-        if (!in_array($clientIp, $allowedIps)) {
+        if (!app()->isLocal() && !in_array($clientIp, $allowedIps)) {
             Log::info('IP address {ip} is not in the list of allowed addresses', ['ip' => $clientIp]);
             abort(403, 'Access denied: Your IP address is not allowed to access this resource.');
         }
